07719ee5bc73d45719950a8ed508b6747ed1162c
Vyshnav.K <vyshnavk117@gmail.com>
2023-12-24 15:19:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './app.scss'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Parallax from './components/parallax/Parallax'
+import Services from './components/services/Services'
+import Cursor from './components/mouse/Cursor'
+import Contact from './components/contact/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <section id='Homepage'>
+        <Cursor />
+        <Hero />
+        <Navbar />
+      </section>
+      <section id='Services'><Parallax type='services'/></section>
+      <section><Services /></section>
+      <section id='Portfolio'><Parallax type='portfolio'/></section>
+      <section id='Contact'><Contact /></section>
+    </div>
+  )
+}
+
+export default App
